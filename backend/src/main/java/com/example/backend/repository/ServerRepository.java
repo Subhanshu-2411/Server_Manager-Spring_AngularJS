5b873c8d950167abba456d1b2af574462faa4a78
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-05-19 19:27:16 +0530

    Completed Domain / Model Repository

---
+++ backend/src/main/java/com/example/backend/repository/ServerRepository.java
@@ -0,0 +1,8 @@
+package com.example.backend.repository;
+
+import com.example.backend.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepository extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
