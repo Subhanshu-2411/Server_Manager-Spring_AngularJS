1d88a2a2e84cfcf259e8059f53cc035d4a77aaf1
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-05-19 21:50:56 +0530

    Completed Service Implementation and HTTP Response Mapping

--- backend/src/main/java/com/example/backend/service/implementation/ServerServiceImplementation.java
+++ backend/src/main/java/com/example/backend/service/implementation/ServerServiceImplementation.java
@@ -1,38 +1,83 @@
 package com.example.backend.service.implementation;
 
+import com.example.backend.enumeration.Status;
 import com.example.backend.model.Server;
+import com.example.backend.repository.ServerRepository;
 import com.example.backend.service.ServerService;
+import jakarta.servlet.Servlet;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
 
+import static java.lang.Boolean.TRUE;
+import static org.springframework.data.domain.PageRequest.of;
+
+@RequiredArgsConstructor // Create a Constructor
+@Service // Service Class
+@Transactional // Transactional
+@Slf4j // Print in console
 public class ServerServiceImplementation implements ServerService {
+
+    private final ServerRepository serverRepository;
+
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Creating New Server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepository.save(server);
     }
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching All Servers");
+        return serverRepository.findAll(of(0, limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching Server: {}", id);
+        return serverRepository.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating Server: {}", server.getName());
+        return serverRepository.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting ServerID: {}", id);
+        serverRepository.deleteById(id);
+        return TRUE;
     }
 
+    @SneakyThrows
     @Override
     public Server ping(String ipAddress) {
-        return null;
+        log.info("Pinging Server at: {}", ipAddress);
+        Server server = serverRepository.findByIpAddress(ipAddress);
+        InetAddress inetAddress = InetAddress.getByName(ipAddress);
+        server.setStatus(inetAddress.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        serverRepository.save(server);
+        return server;
+    }
+
+    private String setServerImageUrl() {
+        String[] imageNames = {
+                "Server1.png",
+                "Server2.png",
+                "Server3.png",
+                "Server4.png",
+        };
+
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imageNames[new Random().nextInt(4)]).toUriString();
     }
 }
